1bf3c7f3a5643e996a57c3b3a525a729c9b2a4df
Rich Brown <richb.hanover@gmail.com>
2024-10-04 20:35:10 -0400

    Evolving - time to push to github

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ require_once 'Parsedown.php';
 //     return file_get_contents($filename);
 // }
 
+// Read the HTML template
+$html_template = file_get_contents('public/index.html');
+
+
 // Read the Markdown file
 $markdown_content = file_get_contents('./index.md');
 
@@ -20,6 +24,9 @@ $markdown_content = str_replace('{passphrase}', $passphrase, $markdown_content);
 $Parsedown = new Parsedown();
 $html_content = $Parsedown->text($markdown_content);
 
+// Plop the $html_content into the $html_template
+$html_template = str_replace('{body}', $html_content, $html_template)
+
 // Output the HTML
-echo $html_content;
+echo $html_template;
 ?>
