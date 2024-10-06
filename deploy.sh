0f1a37b9e32fc79280ccc5b9066a7c8cb7cdc7bb
Rich Brown <richb.hanover@gmail.com>
2024-10-06 10:20:32 -0400

    Update order of steps in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 # Script to deploy the repo files to production
 # It's a hassle to pull the repo to the production directory
-# Instead, pull to a local directory in `deploy` 
-# update the passphrase and restart the netserver,
-# then copy the files to /home/netperf/public_html
+# Instead, pull to a local directory in `deploy`,
+# copy the files to /home/netperf/public_html,
+# then update the passphrase and restart the netserver
 
 # Log into deploy account, cd to Netperf-with-passphrase
 # then use this script
 
 git pull
-sudo sh /home/netperf/public_html/update_passphrase.sh
 sudo cp -r * /home/netperf/public_html/
+sudo sh /home/netperf/public_html/update_passphrase.sh
